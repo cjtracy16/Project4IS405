1dbca0206be7734de9b7600e5712e8ba13b1b505
buhlerdm <dmitchellbuhler@gmail.com>
2022-10-03 15:56:50 -0600

    Testing Milestone 2

--- Wordle.py
+++ Wordle.py
@@ -29,9 +29,30 @@ def wordle():
         gw.set_square_letter(0,i,letterList[i])
 
     def enter_action(s):
-        gw.show_message("You have to implement this method.")
+        # Get row from
+        row = gw.get_current_row()
 
-    
+        # Get word from window:
+        wordLeters = []
+        for col in range(0, 5):
+            wordLeters.append(gw.get_square_letter(row, col))
+
+        word = ''.join(wordLeters)
+        word = word.lower()
+
+        # Checks to see if the entered word is a legitimate word
+
+        # If NOT a legitmate word then display: "Not in word list
+        if word in FIVE_LETTER_WORDS:
+            # If legitimate word then display a positive message
+            gw.show_message("You have to implement this method.")
+
+        else:
+            gw.show_message("Not in word list.")
+
+        
+
+    gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
 
 # Startup code
